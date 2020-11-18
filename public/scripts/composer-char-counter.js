a25eeb365913474878e3e7f1e94d9973a0d59135
remy29 <arthur.d.remy@gmail.com>
2020-11-18 00:22:53 +0000

    added jquery functionality turning counter red under 0

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,17 @@
-// DOM event listener that changes the value assigned to counter HTML element
+// DOM event listener that changes the value assigned to counter HTML element and turns it red if val < 0
 $(document).ready(function() {
   $('textarea').on('input', () => {
     const $input = $('#tweet-text');
     const value = $input.val();
     const remainingChar = 140 - value.length
     $('.container').find('#counter')[0]['innerHTML'] = remainingChar;
+
+    if (remainingChar < 0) {
+        $('#counter').removeClass('counterB').addClass('counterR')
+    }
+    
+    if (remainingChar >= 0 && $('#counter').attr('class') === 'counterR') {
+      $('#counter').removeClass('counterR').addClass('counterB')
+    }
   })
 });
