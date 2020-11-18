7a09336c9bcc6ba757c0091f1af2991cc83711e2
remy29 <arthur.d.remy@gmail.com>
2020-11-18 19:40:25 +0000

    refactored hover function totarget only one tweet

--- public/scripts/tweet-hover.js
+++ public/scripts/tweet-hover.js
@@ -1,24 +1,28 @@
 $(document).ready(function() { 
-  const $username = $('article').children().first().children().first().next()
-  const $usertag = $('article').children().first().children().last()
-  const $tweetText = $('article').children().first().next()
-  const $footerInfo = $('article').children().last().children().first()
-  
-  $('article').on('mouseover', () => {
-    $('article').addClass('hover');
+
+// adds and removes .hover class to various html elements to change styling when mouse enters tweet
+  $('article').on('mouseover', function() { 
+    const $username = $(this).children().first().children().first().next()
+    const $usertag = $(this).children().first().children().last()
+    const $tweetText = $(this).children().first().next()
+    const $footerInfo = $(this).children().last().children().first()
+    $(this).addClass('hover');
     $username.addClass('hover');
     $usertag.addClass('hover');
     $tweetText.addClass('hover');
     $footerInfo.addClass('hover');
   });
 
-  $('article').on('mouseout', () => {
-    $('article').removeClass('hover');
+  $('article').on('mouseout', function() {
+    const $username = $(this).children().first().children().first().next()
+    const $usertag = $(this).children().first().children().last()
+    const $tweetText = $(this).children().first().next()
+    const $footerInfo = $(this).children().last().children().first()
+    $(this).removeClass('hover');
     $username.removeClass('hover');
     $usertag.removeClass('hover');
     $tweetText.removeClass('hover');
     $footerInfo.removeClass('hover');
-  
   });
 
   
